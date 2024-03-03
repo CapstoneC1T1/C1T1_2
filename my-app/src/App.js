8a0f37ffdb09e9b50601ed833c2e0346f7c6abf3
AlexisL321 <liu2256@wisc.edu>
2024-03-03 17:41:42 -0600

    added title to the page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import React from 'react';
 import TransactionList from './components/TransactionList';
 import Header from './components/Header';
+import PageTitle from './components/PageInfo';
 
 function App() {
   return (
     <div className="App">
       <Header />
+	  <PageTitle title = "Checking Account" amount = "$1000"/>
       <TransactionList />
     </div>
   );
