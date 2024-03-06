787be4c7b59266311ec032ebbf8732320583dbed
Hriday <hridaygupta45@gmail.com>
2024-03-06 17:06:42 -0600

    beautified navbar

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import '../styles/Header.css';
 import logo from '../images/Capital-One-Logo.png';
-import Navigation from '../pages/Navbar'
+import Navigation from '../pages/Navbar';
+import Image from 'react-bootstrap/Image';
+import { Col, Container, Row } from 'react-bootstrap';
 
 function Header() {
   return (
-    <header className="header">
-      <img src={logo} className="App-logo" alt="logo"/>
-      <Navigation></Navigation>
-    </header>
-
+    <Container className="header">
+      <Row>
+        <Col lg={12} >
+          <Image src={logo} height={60}/>
+        </Col>
+        <Col>
+          <Navigation className="header "></Navigation>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- my-app/src/pages/Navbar.js
+++ my-app/src/pages/Navbar.js
@@ -7,8 +7,8 @@ function Navigation() {
     return <Navbar bg="dark" variant="dark" sticky="top" expand="sm" collapseOnSelect>
         <Container>
                 <Nav>
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <br></br>
+                    <Nav.Link as={Link} to="/" style={{paddingRight: 10}}>Home</Nav.Link>
+                    {/* <br></br> */}
                     <Nav.Link as={Link} to="/easy-mode">Easy Mode Page</Nav.Link>
                 </Nav>
         </Container>
